65d0c1934ecda5885e0d0930712b22e584fc143e
A.S.Rawat <adityasrwt5@gmail.com>
2023-02-02 16:58:46 +0530

    Update

--- Maze Runner/Assets/Script/CmOV.cs
+++ Maze Runner/Assets/Script/CmOV.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class CmOV : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer entrance;
-    private float sub;
+
     [SerializeField] private Transform player;
-    private void Start()
-    {
-        sub = 1;
-    }
+
     void Update()
     {
         entrance.color = new Color(0, 0, 0, Random.Range(0, 100) * Time.deltaTime);
